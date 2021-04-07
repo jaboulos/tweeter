d5b62b094365e5c086cf614d0a933a97e21e6926
jaboulos <jab12288@gmail.com>
2021-04-07 16:26:24 -0700

    Update query name to users

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -22,7 +22,7 @@ export const DateTime = asNexusMethod(GraphQLDateTime, 'date')
 const Query = objectType({
   name: 'Query',
   definition(t) {
-    t.nonNull.list.nonNull.field('allUsers', {
+    t.nonNull.list.nonNull.field('users', {
       type: 'User',
       resolve: (_parent, _args, context: Context) => {
         return context.prisma.user.findMany()
